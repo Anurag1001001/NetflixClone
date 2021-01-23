90748786dd2962dc1185d2e9fbdf2b48dbe25fe5
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-23 19:01:11 +0530

    displaying jumbodata
    created jumbotron.js inside container and iterating over jumbodata and passing it to a jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React from "react";
 import jumbotron from "./fixtures/jumbo";
-import Jumbotron from "./components/jumbotron";
+import { JumbotronContainer } from "./containers/jumbotron";
 
-function App() {
-  return (
-    <Jumbotron.Container>
-      {jumbotron.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          {/*Here we're using the concept of composition and pops(or you can say in your word props and props.children(if you forget about this go and read composition and inheritance from official documentation))  */}
-          {/* so basically whatever passed b/w <Jumbotron>....</Jumbotron> is the children of jumbotron and passed as a props */}
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+export default function App() {
+  return <JumbotronContainer />;
 }
-
-export default App;
