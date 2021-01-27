cc1acb41cded80497882662efb2158201ed07743
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-27 12:51:19 +0530

    created Home component and renders footer, faqs, jumbotron components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+// Package import
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// File import
+import { Home, Signup, Signin, Browse } from "./pages";
+import * as ROUTES from "./constants/routes";
+
+// import { JumbotronContainer } from "./containers/jumbotron";
+// import { FooterContainer } from "./containers/footer";
+// import { FaqsContainer } from "./containers/faqs";
 
 export default function App() {
   return (
-    <>
+    <Router>
+      <Route exact path="/user">
+        <p>Hello Im a User</p>
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
+  );
+}
+
+{
+  /* <>
       <JumbotronContainer />
       <FaqsContainer />
 
       <FooterContainer />
-    </>
-  );
+    </> */
 }
