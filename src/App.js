7a0a4ee76b67fc804b6380379c770ac866aa8ac2
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-31 19:38:42 +0530

    created an auth listener

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // Package import
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 // File import
 import { Home, Signup, Signin, Browse } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import useAuthListener from "./hooks/use-auth-listener";
 
 export default function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router>
       <Route exact path="/user">
--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,23 +1,42 @@
 import { useState, useEffect, useContext } from "react";
-import { FirebaseContext, firebaseContext } from "../context/firebase";
+import { FirebaseContext } from "../context/firebase";
+
+// ************************************************************************
+
+// but here's a hooks based solution to remove(unmount) the new firebase onAuthStateChanged listener in react
+
+// React.useEffect(() => {
+//   const unsubscribe = firebase.auth().onAuthStateChanged((user) => { // detaching the listener
+//       if (user) {
+//           // ...your code to handle authenticated users.
+//       } else {
+//           // No user is signed in...code to handle unauthenticated users.
+//       }
+//   });
+//   return () => unsubscribe(); // unsubscribing from the listener when the component is unmounting.
+// }, []);
+
+// ************************************************************************
 
 export default function useAuthListener() {
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("authUser"))
   );
   const { firebase } = useContext(FirebaseContext);
-}
 
-useEffect(() => {
-  const Listener = firebase.auth().onAuthStateChange((authUser) => {
-    if (authUser) {
-      localStorage.setItem("authUser", JSON.stringify(authUser));
-      setUser(authUser);
-    } else {
-      localStorage.removeItem("authUser");
-      setUser(null);
-    }
-  });
-  return () => listener();
-}, []);
-return { user };
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        localStorage.setItem("authUser", JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        localStorage.removeItem("authUser");
+        setUser(null);
+      }
+    });
+
+    return () => listener(); // unsubscribing from the listener when the component is unmounting.
+  }, []);
+
+  return { user };
+}
