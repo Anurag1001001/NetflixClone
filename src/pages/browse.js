d07b79ee5e67bbec6a3f77d20f2d112124ff29b7
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-31 23:00:53 +0530

    Fetching films and series from firebase

---
+++ src/pages/browse.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import BrowseContainer from "../containers/browse";
+import useContent from "../hooks/use-content";
+// import { selectionFilter } from "../utils";
+
+export default function Browse() {
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+  // const { slides } = selectionFilter({ series, films });
+
+  return <BrowseContainer />;
+}
